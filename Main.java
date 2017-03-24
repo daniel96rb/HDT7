1dfcef9560fbc56e0776428bf5ed668ddf01dc07
daniel96rb <drodriguezbianchi96@gmail.com>
2017-03-23 23:47:52 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import java.util.List;
 /**
 * Clase: Main.java
 * @author Daniel Rodriguez, Ana lucia Leppe
+* Clase Main donde se utilizan el bosque binario para realizar una busqueda de palabras traducidas del ingles para
+* realizar una traduccion de un archivo teniendo un documento de dicccionario de asociaciones.
 */
 
 public class Main {
@@ -13,29 +15,57 @@ public class Main {
 
 	public static void main(String[] args){
 
-		//Se crea el arbol y arrays de la clase Association
+		//Se realizan las instancias del arbol y el arreglo de la Association
 		BinaryTree arbol = new BinaryTree();
-		Association<String,String> [] miLista = new Association[36];
-      Association temp = new Association();
-     	int count=0;
+		Association<String,String> [] miLista = new Association[39];
+    Association assoc = new Association();
+    int conteo = 0;
 
-     	//Se lee el diccionario y se ingresa al arbol
-	    String diccionario = temp.leerArchivo("diccionario.txt");
+     	//Se realiza la lectura del diccionario y se ingresa al arbol
+	    String diccionario = assoc.lectura("diccionario.txt");
 	    for (String sp : diccionario.split("-")) {
 		String st[] = sp.split(",");
-		miLista[count] = new Association<String, String>(st[0].substring(1), st[1].substring(1, st[1].length()-1));
-		arbol.insertNode(miLista[count]);
-		count ++;
+		miLista[conteo] = new Association<String, String>(st[0].substring(1), st[1].substring(1, st[1].length()-1));
+		arbol.insertNode(miLista[conteo]);
+		conteo ++;
         }
 
-	   if(miLista[0].compareTo(miLista[1])>1){
-			 nodo2.setValue(miLista[1]);
-			 nodo1.setRight(nodo2);
-		 }else{
-			 nodo2.setValue(miLista[1]);
-			 nodo1.setLeft(nodo2);
-		 }
+	  //Atraviesa el arbol
+	  arbol.busquedaInOrder();
+
+	  String archivo = assoc.lectura("archivo.txt");
+	  String textoATraducir = archivo.substring(0, archivo.length()-1);
+	  System.out.println("La conversacion en ingles es: \n"+textoATraducir+"\n");
 
 
+		//Realiza el recorrimiento del texto A Traducir
+		int inicio = 0;
+		int num=0;
+		List<String> Text = new ArrayList<>();
+		for(int i = 0; i <textoATraducir.length(); i++){
+			if(Character.toString(textoATraducir.charAt(i)).equals(" ") | Character.toString(textoATraducir.charAt(i)).equals(".")){
+				Text.add(textoATraducir.substring(inicio, i));
+				inicio = i;
+				num++;
+			}
+		}
+
+    //Con los metodos del BinaryTree de busqueda se realizan las traducciones.
+    String resultado = "";
+    for(int n = 0; n<num; n++){
+    		String palabra = arbol.Busqueda(Text.get(n).replace(" ",""));
+        String palabraSegunda = Text.get(n).replace(" ","");
+
+      	if(palabra.equals(palabraSegunda)){
+      			resultado = resultado+" "+Text.get(n).replace(" ","")+" ";
+
+        }
+        else{
+					resultado=resultado+arbol.Busqueda(Text.get(n).replace(" ","")+"")+" ";
+				}
+   }
+
+  //Imprime la Traduccion
+  System.out.println("La conversacion en espaniol es: \n"+resultado+"\n");
 	}
 }
